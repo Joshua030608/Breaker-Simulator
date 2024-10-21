b52af5ec700eb97279a36d28f206126acde30884
Joshua030608 <64506906+Joshua030608@users.noreply.github.com>
2024-10-20 21:53:08 -0400

    Initial Commit

---
+++ Breaker Simulator/Breaker_SimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Breaker_SimulatorApp.swift
+//  Breaker Simulator
+//
+//  Created by Joshua Ford on 10/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Breaker_SimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
