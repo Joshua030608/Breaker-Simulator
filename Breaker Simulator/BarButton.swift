81a3eeef30c49ccde4657d2e473beaed74facb90
Joshua030608 <64506906+Joshua030608@users.noreply.github.com>
2024-10-22 10:20:48 -0400

    Refactored Code, removed GIF
    Refactored some code to clean up main views, removed gif, and began custom hub views.

---
+++ Breaker Simulator/BarButton.swift
@@ -0,0 +1,54 @@
+//
+//  BarButton.swift
+//  Breaker Simulator
+//
+//  Created by Joshua Ford on 10/22/24.
+//
+
+import SwiftUI
+
+enum BarButtonType: CaseIterable {
+    
+    static let allCases: [BarButtonType] = [.back, .settings]
+    
+    case back
+    case settings
+}
+
+struct BarButton: View {
+    @Environment(\.dismiss) private var dismiss
+    
+    let type: BarButtonType
+    
+    init (type: BarButtonType) {
+        self.type = type
+    }
+    
+    var body: some View {
+        switch type {
+            case .settings:
+            Button {
+                print("Settings Pressed")
+            } label: {
+                Image(systemName: "gear.circle.fill")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+            }
+                .padding(.trailing, 20)
+                .padding(.top, 50)
+                .offset(x: 165, y: -35)
+            case .back:
+            Button {
+                dismiss()
+            } label: {
+                Image(systemName: "arrow.left.circle.fill")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+            }
+                .padding(.leading, 20)
+                .padding(.top, 50)
+                .offset(x: -165, y: -35)
+        }
+    }
+    
+}
--- Breaker Simulator/HubView.swift
+++ Breaker Simulator/HubView.swift
@@ -5,13 +5,19 @@
 //  Created by Joshua Ford on 10/21/24.
 //
 import SwiftUI
-import SDWebImageSwiftUI
+
+enum HubViewType {
+    case live
+    case inventory
+    case shipping
+}
 
 struct HubView: View {
     @State var hasPlayedBefore: Bool
     
     let goldColor: Color = Color(CGColor(red: 239/255, green: 191/255, blue: 4/255, alpha: 1))
     let darkGoldColor: Color = Color(CGColor(red: 173/255, green: 139/255, blue: 3/255, alpha: 1))
+    @State var currentView: HubViewType = .live
     
     init(hasPlayedBefore: Bool) {
         self.hasPlayedBefore = hasPlayedBefore
@@ -23,49 +29,37 @@ struct HubView: View {
                 Color.gray
                     .ignoresSafeArea()
                 VStack {
-                    BackButton()
-                        .padding(.leading, 20)
-                        .padding(.top, 50)
-                        .offset(x: -185, y: -35)
-                    //Text("Main Screen")
-                    AnimatedImage(name: "cards.gif")
-                        .resizable()
-                        .frame(width: 200, height: 200)
-                    Spacer()
-                    ZStack {
-                        Rectangle()
-                            .ignoresSafeArea()
-                            .frame(maxWidth: .infinity, minHeight: 80, maxHeight: 80)
-                            .foregroundStyle(Color(goldColor))
-                        Rectangle()
-                            .frame(maxWidth: .infinity, minHeight: 15, maxHeight: 15)
-                            .offset(y: -32.5)
-                            .foregroundStyle(Color(darkGoldColor))
-                        ForEach(0..<2) { index in
-                            let position = CGFloat((Int(geometry.size.width) * (index + 1)) / 3)
+                    switch currentView {
+                    case .live:
+                        HStack {
+                            ForEach(0..<2) { index in
+                                BarButton(type: BarButtonType.allCases[index])
+                            }
+                        }
+                        Spacer()
+                        ZStack {
                             Rectangle()
                                 .ignoresSafeArea()
-                                .frame(width: 7, height: 80)
-                                .position(x: position)
-                                .offset(y: 40)
+                                .frame(maxWidth: .infinity, minHeight: 80, maxHeight: 80)
+                                .foregroundStyle(Color(goldColor))
+                            Rectangle()
+                                .frame(maxWidth: .infinity, minHeight: 15, maxHeight: 15)
+                                .offset(y: -32.5)
                                 .foregroundStyle(Color(darkGoldColor))
-                        }
-                    }.frame(maxHeight: 80)
+                            ForEach(0..<2) { index in
+                                let position = CGFloat((Int(geometry.size.width) * (index + 1)) / 3)
+                                Rectangle()
+                                    .ignoresSafeArea()
+                                    .frame(width: 7, height: 80)
+                                    .position(x: position)
+                                    .offset(y: 40)
+                                    .foregroundStyle(Color(darkGoldColor))
+                            }
+                        }.frame(maxHeight: 80)
+                    default: EmptyView()
+                    }
                 }
             }
-        }
-    }
-}
-
-struct BackButton: View {
-    @Environment(\.dismiss) private var dismiss
-    var body: some View {
-        Button {
-            dismiss()
-        } label: {
-            Image(systemName: "arrow.left.circle.fill")
-                .resizable()
-                .frame(width: 30, height: 30)
         }
     }
 }
